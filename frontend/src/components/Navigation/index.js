5000389c1ac00eb162943dae5d87d9df335189b0
Mei Shih <shihmeiyin@gmail.com>
2022-02-16 13:54:23 -0800

    Splash page mvp done

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
+import tickety_logo from '../../images/tickety_logo.png'
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -16,19 +17,22 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
+        <div className='navbar-sign-up-btn'>
+          <NavLink to="/signup">Sign Up</NavLink>
+        </div>
         <LoginFormModal />
-        <NavLink to="/signup">Sign Up</NavLink>
       </>
     );
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    <nav className='navbar-top'>
+          <NavLink exact to="/" className='navbar-logo'>
+            <span><img src={tickety_logo} alt='logo'></img></span>
+            Tickety
+          </NavLink>
+          {isLoaded && sessionLinks}
+    </nav>
   );
 }
 
---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,31 @@
+import { NavLink } from 'react-router-dom';
+import './SplashPage.css'
+import banner_img from '../../images/Flow.png';
+import splash_img1 from '../../images/Team.png'
+const SplashPage = () => {
+    return (
+    <div className='splash-page-content'>
+        <div className='banner'>
+            <div className='banner-text'>
+                <h1>Tickety made project managment <span>easy</span></h1>
+                <p>With tickety, you can manage your work flow by stage, keep track of your tasks, and assign tasks to your teammates.</p>
+                <NavLink className='banner-sign-up-btn' to="/signup">Sign Up for free</NavLink>
+            </div>
+            <div className='banner-img'>
+                <img src={banner_img}></img>
+            </div>
+        </div>
+        <div className='section-one'>
+            <div className='section-one-text'>
+                <h2>Tickety improve <span>productivity</span></h2>
+                <p>Organize your tasks all in one place. Customize task cards for easy analysis and improve productivity.</p>
+            </div>
+            <div className='section-one-img'>
+                <img src={splash_img1}></img>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default SplashPage;
