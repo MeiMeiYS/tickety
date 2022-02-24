f62af6615f6830aa5d3c72ea2a8af936ed86fe9a
Mei Shih <shihmeiyin@gmail.com>
2022-02-24 10:13:52 -0800

    Updated about this app page and added up comming features

--- frontend/src/components/SplashPage/Dashboard/MainContent/DashboardHome/index.js
+++ frontend/src/components/SplashPage/Dashboard/MainContent/DashboardHome/index.js
@@ -17,6 +17,11 @@ const DashboardHome = () => {
     window.open("https://meiys.me/");
   };
 
+  const goToRepo = (e) => {
+    e.preventDefault();
+    window.open("https://github.com/MeiMeiYS/tickety");
+  };
+
   return (
     <>
       <div className="main-content-card about-creator">
@@ -54,14 +59,47 @@ const DashboardHome = () => {
         <h2>About this app</h2>
         <div className="about-content">
           <p>
-            Tickety app is designed & coded by Mei Shih using React as frontend,
-            Express as backend, and used PostgreSQL/Sequelize to setup database.
-            Tickety app is built in 10 days in 2022 February. It is Mei's
-            capstone project at App Academy. The most challenging part of
-            building this app for Mei is implementing the drag and drop task
-            card on the kanban board and dynamically updating the frontend state
-            and backend data.
+            Tickety is an app where you can manage your work flow by stages and
+            keep track of your tasks. Tickety app is designed & coded by Mei
+            Shih using React as frontend, Express as backend, and used
+            PostgreSQL/Sequelize to setup database. Tickety app is built in 10
+            days in 2022 February. It is Mei's capstone project at App Academy.
+            The most challenging part of building this app for Mei is
+            implementing the drag and drop task card on the kanban board and
+            dynamically updating the frontend state and backend data with no
+            animation delay. Check out the Tickety GitHub repo{" "}
+            <span className="repo-link" onClick={goToRepo}>
+              here
+            </span>
+            .
           </p>
+        </div>
+      </div>
+      <div className="main-content-card about-this-app">
+        <h2>Future updates</h2>
+        <div className="about-content">
+          <p>Here are some up coming features in the near future:</p>
+          <ul className='future-updates'>
+            <li>
+              You will be able to invite teammates via email or username to edit
+              your projects.
+            </li>
+            <li>
+              You can assign task to any teammate and their avatar will show on
+              the task card.
+            </li>
+            <li>You can set color of every single task card.</li>
+            <li>You can see who is the last user to update a task card.</li>
+            <li>
+              You will get notification when teammates create/edit/delete your
+              project.
+            </li>
+            <li>
+              You can archive a project or kanban. The Archived project or
+              kanban will be in the archived section.
+            </li>
+            <li>Teammates can comment on task card.</li>
+          </ul>
         </div>
       </div>
     </>
